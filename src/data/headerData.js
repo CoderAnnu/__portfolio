b0547c51bdf5f4bfdee4688f14442b70e2dc77fa
coderannu <akrajput01206@gmail.com>
2022-12-28 12:20:05 +0530

    image  update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Coderannu',
     title: "Full Stack Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://github.com/CoderAnnu/portfolio/blob/master/src/assets/svg/contacts/default.jpg',
+    image: 'https://github.com/CoderAnnu/portfolio/blob/master/src/assets/svg/contacts/default.jpg?raw=true',
     resumePdf: resume
 }
