c177966149126c4667eff5bf88d5698f03ab056f
coderannu <akrajput01206@gmail.com>
2022-12-28 12:11:52 +0530

    text update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Coderannu',
-    title: "Web Developer",
+    title: "Full Stack Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: 'https://github.com/CoderAnnu/portfolio/blob/master/src/assets/svg/contacts/default.jpg',
     resumePdf: resume
